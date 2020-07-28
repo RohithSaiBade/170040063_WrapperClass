7028b89ed1e4105d88fc1a30fa805c35ad8ebc25
Rohit <Rohit@DESKTOP-S97OQKA>
2020-07-28 10:17:11 +0530

    Wrapper Classes

---
+++ WrapperClass/src/Prblm2.java
@@ -0,0 +1,9 @@
+public class Prblm2 {
+  public static void main(String args[]) {
+    int a=Integer.parseInt(args[0]);
+    System.out.println("Given number :"+a);
+    System.out.println("Binary equivalent :"+Integer.toBinaryString(a));
+    System.out.println("Octal equivalent :" +Integer.toOctalString(a));
+    System.out.println("Hexadecimal equivalent :" +Integer.toHexString(a));
+  }
+}
